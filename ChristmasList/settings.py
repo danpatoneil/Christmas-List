d51b4e17b09b2594a6aa44b0d19eade1440c4a94
danpatoneil <danpatoneil@gmail.com>
2025-01-20 17:42:13 -0500

    fixing procfile for heroku

--- ChristmasList/settings.py
+++ ChristmasList/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT =  BASE_DIR/'media'
+MEDIA_ROOT =  BASE_DIR/'staticfiles'
 MEDIA_URL = '/media/'
 
 DATE_FORMAT = 'M j, Y'
